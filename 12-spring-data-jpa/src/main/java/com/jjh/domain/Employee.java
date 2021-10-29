b45290b80a934a530b1cb0717544abdb0ebe05f8
John Hunt <john.hunt10@gmail.com>
2021-10-29 15:04:15 +0100

    JPA Example

---
+++ 12-spring-data-jpa/src/main/java/com/jjh/domain/Employee.java
@@ -0,0 +1,42 @@
+package com.jjh.domain;
+
+import javax.persistence.*;
+
+@Entity(name="employee")
+public class Employee {
+
+	@Column(name="name")
+	private String name;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	public Employee() {
+	}
+
+	public Employee(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String toString() {
+		return name + ": " + id;
+	}
+}
