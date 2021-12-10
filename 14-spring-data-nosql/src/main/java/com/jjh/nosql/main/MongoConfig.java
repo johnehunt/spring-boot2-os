d937f1f5de6c8e7f810efce2a4e12ef96b48c6fb
John Hunt <john.hunt10@gmail.com>
2021-12-10 16:02:22 +0000

    Minor fixes

--- 14-spring-data-nosql/src/main/java/com/jjh/nosql/main/MongoConfig.java
+++ 14-spring-data-nosql/src/main/java/com/jjh/nosql/main/MongoConfig.java
@@ -23,7 +23,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
